ed80f8034b2b0e5e324269f3e51f953336406fe4
Aniket2659 <aniket2659@gmail.com>
2024-07-16 20:36:48 +0530

    [aniket] add: program to add multiple address book

--- address_book.py
+++ address_book.py
@@ -6,6 +6,9 @@ class AddressBook:
         print("1. Add the contacts in address book")
         print("2. Edit a contact in the address book")
         print("3. Delete the existing contact")
+        print("4. Display the contact ")
+        print("5. Enter name of address book")
+        print("6. Display address book")
         choice = int(input("Enter your choice: "))
         return choice
 
@@ -30,6 +33,7 @@ class AddressBook:
             'email': email
         }
         self.contacts.append(contact)
+
     def edit_contact(self,contact):
         for contact in self.contacts:
             contact['first_name'] = input('enter new first name ')
@@ -40,6 +44,7 @@ class AddressBook:
             contact['zip_code'] = int(input('enter new zip code '))
             contact['phone_number'] = int(input('enter new phone number '))
             contact['email'] = input('enter new email address')
+
     def del_contact(self):
         full_name1 = input('enter the full name of the contact to delete: ')
         for contact in self.contacts:
@@ -70,15 +75,40 @@ class AddressBook:
                     print('there is no existing contact')
             case 3:
                 self.del_contact()
+            case 4:
+                self.display_contacts()
+            case 5:
+                m_address_book.create_address_book()
+            case 6:
+                m_address_book.display_address_book()
             case _:
                 print("invalid choice please try again.")
 
+
+class multiple_address_book(AddressBook):
+    def __init__(self):
+        super().__init__()
+
+        self.address_books = {}
+    
+    def create_address_book(self):
+        name_address_book=input('enter the name of address book to create:')
+        if name_address_book not in self.address_books:
+            self.address_books[name_address_book]=address.contacts
+            
+    
+    def display_address_book(self):
+        print(self.address_books)
+
+
+
 if __name__ == "__main__":
-    address_book = AddressBook()
+    address = AddressBook()
+    m_address_book=multiple_address_book()
     while True:
-        user_choice = address_book.menu()
-        address_book.selection(user_choice)
-        address_book.display_contacts()
+        user_choice = address.menu()
+        address.selection(user_choice)
+
         exit=input('do you want to continue(yes/no)')
         if exit=='no':
             break
